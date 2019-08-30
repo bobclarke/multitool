78f0962f3cb3b179bddce62a77299bdc6ec63030
Bob Clarke <bobmclarke@gmail.com>
2019-08-31 00:59:57 +0100

    correct compose and createdb stuff

--- docker-compose/createdb.sql
+++ docker-compose/createdb.sql
@@ -4,5 +4,6 @@ CREATE DATABASE bird_encyclopedia;
 CREATE TABLE birds (
   id SERIAL PRIMARY KEY,
   bird VARCHAR(256),
-  description VARCHAR(1024)
+  description VARCHAR(1024),
+  species VARCHAR(256)
 );
